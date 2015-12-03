439650d377130cdeb909b26f1a4edc1bc6f9da40
Legoboy0215 <legoboy0215weebly@gmail.com>
2015-12-03 20:24:28 +0800

    lol

--- src/Legoboy/ST/Loader.php
+++ src/Legoboy/ST/Loader.php
@@ -27,7 +27,7 @@ class Loader extends PluginBase{
 	
 	public function process(){
 		$path = $this->getServer()->getPluginManager()->getPlugin("SimpleAuth")->getDataFolder() . "/players/";
-		foreach(glob($path. "*/*.yml") as $file){
+		foreach(glob($path . "*/*.yml") as $file){
 			$data = new Config($file, Config::YAML);
 			$pname = trim(strtolower(basename($file, ".yml")));
 			$regdate = $data->get("registerdate");
